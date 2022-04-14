4590f800b3831c2baf564903ce2c058de4ee75d5
ClaudiaBlancoM <cabm0626@gmail.com>
2022-04-14 16:06:35 -0400

    first commit

---
+++ post.sql
@@ -0,0 +1,11 @@
+-- La academia está trabajando en mejorar su plataforma de estudio y planea crear un espacio
+-- que sirva como una red social entre estudiantes y docentes, por lo que necesita almacenar
+-- en una base de datos los post y comentarios que se generen.
+-- ● Parte 1: Creación, inserciones, modificaciones y eliminaciones en la tabla “Posts”.
+-- ● Parte 2: Creación e inserciones en la tabla “comentarios”.
+
+
+
+-- Parte 1
+--* 1. Crear una base de datos con nombre “Posts”. (1 Punto)
+\c claudia
